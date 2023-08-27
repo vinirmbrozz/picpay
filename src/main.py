a12fb1d9e9311a2e530e51110b09319039875382
vinirmbrozz <118769144+vinirmbrozz@users.noreply.github.com>
2023-08-27 12:40:54 -0300

    Criado Endpoint Depositar

--- src/main.py
+++ src/main.py
@@ -25,7 +25,6 @@ class Usuario(BaseModel):
     senha: str
     lojista: bool
 
-
 @app.post("/usuarios")
 def usuario(usuario: Usuario):
     conexao = conectarBanco()
@@ -50,6 +49,38 @@ def usuario(usuario: Usuario):
     conexao.close()
     
     return {"return": "Usuário criado"}
+
+
+@app.post("/depositar")
+def deposito(valor: float, cpf: str):
+    conexao = conectarBanco()
+    cur = conexao.cursor()
+    
+    cur.execute("select * from usuarios where cpf_cnpj = %s", (cpf,))
+    usuario = cur.fetchone()
+    #Verifica se existe um usuário com o mesmo CPF ou e-mail
+    if not usuario:
+        cur.close()
+        conexao.close()
+        raise HTTPException(status_code=400, detail="Usuário não encontrado")
+    
+    #Realizar deposito
+    cur.execute("select * from saldo where id_usuario = %s", (cpf,))
+    saldo = cur.fetchone()
+    if saldo:
+        cur.execute("update saldo set saldo = saldo + %s where id_usuario = %s", (valor, cpf))
+    else:
+        cur.execute("insert into saldo (id_usuario, saldo) values (%s, %s)", (cpf, valor))
+    
+    
+    # Comitar a inserção de usuário e fechar a conexão
+    conexao.commit()    
+
+    
+    cur.close()
+    conexao.close()
+    
+    return {"return": "Depósito realizado com sucesso"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
